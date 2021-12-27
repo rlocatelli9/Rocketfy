351a20717c19e5055e04bac648ce43aae46d1c34
Robson Locatelli <rlocatelli9@gmail.com>
2021-12-27 17:38:58 -0300

    Heder under construction - part 1

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {MdOutlineAlignVerticalTop, MdOutlineExpandMore, MdOutlineSpaceDashboard} from 'react-icons/md'
 
-import { Container } from './styles';
+import { Container, NavBar, Button } from './styles';
 
 function Header() {
+  const [isSelected, setIsSelect] = useState('board')
   return (
     <Container>
       <h1>Rocketfy</h1>
+      <main>
+        <section><p>Desenvolvimento de Software - Agile</p></section>
+        <NavBar isSelected={isSelected}>
+          <ul>
+            <li>
+              <Button type="button">
+                <MdOutlineAlignVerticalTop size={20} color="#FFF"/>
+                <span>Visualizar em Kanban</span>
+                <MdOutlineExpandMore color="#DDD"/>
+              </Button>
+            </li>
+            <li>
+              <Button type="button">
+                <MdOutlineSpaceDashboard size={20} color="#FFF"/>
+                <span>Relatórios</span>
+                <MdOutlineExpandMore color="#DDD"/>
+              </Button>
+            </li>
+          </ul>
+        </NavBar>
+      </main>
+      <aside>
+        <section>Profile</section>
+        <section>actions</section>
+      </aside>
     </Container>
   )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -7,5 +7,63 @@ export const Container = styled.div`
   color: #fff;
 
   display: flex;
+  /* align-items: center; */
+
+  h1 {
+    margin-right: 30px;
+    display: flex;
+    align-items: center;
+  }
+
+  main {
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    justify-content: space-between;
+    padding-top: 10px;
+    section{
+      height: 40px;
+      display: flex;
+      align-items: center;
+    }
+  }
+
+  aside {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-left: auto;
+  }  
+`;
+
+
+export const NavBar = styled.nav`
+  display: flex;
   align-items: center;
+  height: 30px;
+
+  ul {
+    display: flex;
+    li {
+      margin-right: 10px;
+    }
+  }
+`;
+
+export const Button = styled.button`
+  border: none;
+  background: transparent;
+  color: #FFF;
+  
+  display: flex;
+  align-items: center;
+
+  span {
+    margin-left: 5px;
+  }
+
+  &:hover{
+    cursor: pointer;
+  }
 `;
